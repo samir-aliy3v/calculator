6ebace0cb611834ef35bd998efd8dfd547881f86
samir-aliy3v <91435628+samir-aliy3v@users.noreply.github.com>
2021-11-20 21:20:52 +0400

    Delete calculator.js

--- calculator.js
+++
@@ -1,8 +0,0 @@
-var numbers = document.querySelectorAll('.numbers');
-var operators = document.querySelectorAll('.operators');
-var netice = document.getElementById('demo');
-var allClear = document.querySelector('.allClear');
-var del = document.querySelector('.del');
-var equal = document.querySelector('.equal');
-
-
