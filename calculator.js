2aecd487e1a2db52ff1e8e5e5c6e8929156b6574
samir-aliy3v <samir.aliy3v@gmail.com>
2021-11-20 21:18:50 +0400

    calculator project

---
+++ calculator.js
@@ -0,0 +1,8 @@
+var numbers = document.querySelectorAll('.numbers');
+var operators = document.querySelectorAll('.operators');
+var netice = document.getElementById('demo');
+var allClear = document.querySelector('.allClear');
+var del = document.querySelector('.del');
+var equal = document.querySelector('.equal');
+
+
